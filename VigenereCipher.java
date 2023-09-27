18fab884729414ede40ed608210e3f6d2b27983c
Christian Barbosa <christian.barbosa05222001@gmail.com>
2023-09-27 21:00:19 +0800

    Add some changes in this code

--- VigenereCipher.java
+++ VigenereCipher.java
@@ -16,6 +16,22 @@ public class VigenereCipher
 
         encrypt(plaintext, keyword);
         decrypt();
+
+        //if the key is integer execute this condition
+        int intKey;
+        if (isInteger(keyword)) {
+            intKey = Integer.parseInt(keyword);
+            System.out.println("Integer Key: " + intKey);
+            encrypt(plaintext, String.valueOf(intKey));
+            decrypt(plaintext, intKey);
+        } else {
+            System.out.println("String Key: " + keyword);
+            encrypt(plaintext, keyword);
+            decrypt(plaintext, Integer.parseInt(keyword));
+        }
+    }
+
+    private static void decrypt(String plaintext, int intKey) {
     }
 
 
@@ -44,6 +60,7 @@ public class VigenereCipher
         //encryption code
         for(i = 0; i < msgLen; ++i)
             encryptedMsg[i] = (char) (((msg[i] + key[i]) % 26) + 'A');
+
 
 
         System.out.println("Original Message: " + plaintext);
@@ -86,5 +103,14 @@ public class VigenereCipher
 
         System.out.println("Decrypted Message: " + String.valueOf(decryptedMsg));
 
+    }
+    public static boolean isInteger(String str) {
+        try {
+            int value = Integer.parseInt(str);
+            // Ensure the integer value is within the range [0, 25]
+            return value >= 0 && value <= 25;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
