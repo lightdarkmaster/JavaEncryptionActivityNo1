52a51de5ce33ca0d62676634414e57cfaffea7b2
Christian Barbosa <christian.barbosa05222001@gmail.com>
2023-09-30 19:38:42 +0800

    Optimize my Code

---
+++ QuizGame.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+class QuizGame {
+    public static final String[] QUESTIONS = {
+            "Who is the Founder of Apple Incorporation?",
+            "Who is the Founder of Samsung Incorporation?",
+            "Who is the Founder of Nokia Incorporation?",
+            "Who is the Founder of Infinix Incorporation?",
+            "Who is the Founder of Huawei Incorporation?"
+    };
+
+    public static final String[] ANSWERS = {
+            "Steve Jobs",
+            "Christian Barbosa",
+            "Fredrik Idestam",
+            "George Zhu",
+            "Ren Zhengfei"
+    };
+
+    public static int score = 0;
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to our Quiz Game");
+
+        Scanner input = new Scanner(System.in);
+
+        for (int i = 0; i < QUESTIONS.length; i++) {
+            System.out.println(QUESTIONS[i]);
+            String answer = input.nextLine();
+            if (answer.equalsIgnoreCase(ANSWERS[i])) {
+                System.out.println("You got the correct answer!!");
+                score++;
+            } else {
+                System.out.println("Sorry, you got a wrong answer..");
+            }
+        }
+
+        grade();
+        System.out.println("Score: " + score);
+    }
+
+    public static void grade() {
+        if (score == 5) {
+            System.out.println("Wow, you got them all correct!!");
+        } else if (score <= 4 && score > 0) {
+            System.out.println("You did a great job.");
+        } else {
+            System.out.println("You failed");
+        }
+    }
+}
